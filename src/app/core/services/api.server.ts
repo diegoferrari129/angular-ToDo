776d1a7f86531686ca2304e85f6e4c08d9f8b4f7
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2026-04-18 21:14:14 +0200

    feat: environment, api service and cors

---
+++ src/app/core/services/api.server.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface ApiOptions {
+  headers?: HttpHeaders;
+  params?: HttpParams;
+  responseType?: 'json';
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  protected apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+  }
+
+  get<T>(endpoint: string): Observable<T> {
+    return this.http.get<T>(`${this.apiUrl}/${endpoint}`);
+  }
+}
